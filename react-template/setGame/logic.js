9d0eff8b846fe138e970e71209e5d89fc6a168da
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-26 11:14:32 +0200

    full array template implemented

--- react-template/setGame/logic.js
+++ react-template/setGame/logic.js
@@ -17,27 +17,46 @@ class Board extends React.Component {
     render() {
         var array = [
             ["blue", "striped", "circle", 2],
+            ["green", "solid", "square", 3],            
             ["green", "solid", "square", 3],
-            
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+            ["green", "solid", "square", 3],
+
+
+
         ]
 
         return (
             <div>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                <div className="row">
+                    <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[1][0]} cardTexture={array[1][1]} cardShape={array[1][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[2][0]} cardTexture={array[2][1]} cardShape={array[2][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[3][0]} cardTexture={array[3][1]} cardShape={array[3][2]} cardNumber={array[0][3]}></Card>
+                </div>
 
+                <div className="row">
+                    <Card cardColor={array[4][0]} cardTexture={array[4][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[5][0]} cardTexture={array[5][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[6][0]} cardTexture={array[6][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[7][0]} cardTexture={array[7][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                </div>
                 
+                <div className="row">
+                    <Card cardColor={array[8][0]} cardTexture={array[8][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[9][0]} cardTexture={array[9][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[10][0]} cardTexture={array[10][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[11][0]} cardTexture={array[11][0]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                </div>
             </div>
+
         )
     }
 }
