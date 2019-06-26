414048b0d839ecbc0eb017c762464f0d3a6f31ea
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-26 12:03:04 +0200

    react implemented cefi's array

--- react-template/setGame/logic.js
+++ react-template/setGame/logic.js
@@ -16,28 +16,28 @@ class Board extends React.Component {
         super(props);
     }
     render() {
-        var array = set.cards;
+        var array = window.set.chosenCards;
         return (
             <div>
                 <div className="row">
                     <Card cardColor={array[0][0]} cardTexture={array[0][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[1][0]} cardTexture={array[1][1]} cardShape={array[1][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[2][0]} cardTexture={array[2][1]} cardShape={array[2][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[3][0]} cardTexture={array[3][1]} cardShape={array[3][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[1][0]} cardTexture={array[1][1]} cardShape={array[1][2]} cardNumber={array[1][3]}></Card>
+                    <Card cardColor={array[2][0]} cardTexture={array[2][1]} cardShape={array[2][2]} cardNumber={array[2][3]}></Card>
+                    <Card cardColor={array[3][0]} cardTexture={array[3][1]} cardShape={array[3][2]} cardNumber={array[3][3]}></Card>
                 </div>
 
                 <div className="row">
-                    <Card cardColor={array[4][0]} cardTexture={array[4][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[5][0]} cardTexture={array[5][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[6][0]} cardTexture={array[6][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[7][0]} cardTexture={array[7][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[4][0]} cardTexture={array[4][1]} cardShape={array[4][2]} cardNumber={array[4][3]}></Card>
+                    <Card cardColor={array[5][0]} cardTexture={array[5][1]} cardShape={array[5][2]} cardNumber={array[5][3]}></Card>
+                    <Card cardColor={array[6][0]} cardTexture={array[6][1]} cardShape={array[6][2]} cardNumber={array[6][3]}></Card>
+                    <Card cardColor={array[7][0]} cardTexture={array[7][1]} cardShape={array[7][2]} cardNumber={array[7][3]}></Card>
                 </div>
 
                 <div className="row">
-                    <Card cardColor={array[8][0]} cardTexture={array[8][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[9][0]} cardTexture={array[9][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[10][0]} cardTexture={array[10][1]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
-                    <Card cardColor={array[11][0]} cardTexture={array[11][0]} cardShape={array[0][2]} cardNumber={array[0][3]}></Card>
+                    <Card cardColor={array[8][0]} cardTexture={array[8][1]} cardShape={array[8][2]} cardNumber={array[8][3]}></Card>
+                    <Card cardColor={array[9][0]} cardTexture={array[9][1]} cardShape={array[9][2]} cardNumber={array[9][3]}></Card>
+                    <Card cardColor={array[10][0]} cardTexture={array[10][1]} cardShape={array[10][2]} cardNumber={array[10][3]}></Card>
+                    <Card cardColor={array[11][0]} cardTexture={array[11][0]} cardShape={array[11][2]} cardNumber={array[11][3]}></Card>
                 </div>
             </div>
 
