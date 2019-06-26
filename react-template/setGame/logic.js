7f48965ab8b6931381a4c393370028802fab4817
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-26 10:16:40 +0200

    basic board with 12 cards

--- react-template/setGame/logic.js
+++ react-template/setGame/logic.js
@@ -1,4 +1,62 @@
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+            </div>
+        )
+    }
+}
+class Card extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="card">
+            </div>
+        )
+    }
+}
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App/>,
     document.getElementById("root")
-);
+)
+/*
+class Board extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <Card></Card>
+            </div>
+        )
+    }
+}
+class Card extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}*/
